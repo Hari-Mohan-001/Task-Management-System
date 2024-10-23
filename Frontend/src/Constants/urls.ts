b3d145ae65f1680c5d7677528ee3bf176e731c46
Hari Mohan <hari1111mohan@gmail.com>
2024-10-24 00:24:55 +0530

    added urls

--- Frontend/src/Constants/urls.ts
+++ Frontend/src/Constants/urls.ts
@@ -1,2 +1,5 @@
-export const Base_Url = 'http://localhost:3001/api'
-export const socket_Url = "http://localhost:3001"
+// export const Base_Url = 'http://localhost:3001/api'
+// export const socket_Url = "http://localhost:3001"
+
+export const Base_Url = 'https://task-management-system-53lj.onrender.com/api'
+export const socket_Url = "https://task-management-system-53lj.onrender.com"
